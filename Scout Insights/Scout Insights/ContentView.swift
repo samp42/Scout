ea8d9493ff3f4092db849ed1b0e60acb23172156
Samuel Proulx <samuelproulx26@gmail.com>
2022-04-01 00:55:36 -0400

    Initial Commit

---
+++ Scout Insights/Scout Insights/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Scout Insights
+//
+//  Created by Samuel Proulx on 2022-04-01.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Scout Insights/Scout Insights/Scout_InsightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  Scout_InsightsApp.swift
+//  Scout Insights
+//
+//  Created by Samuel Proulx on 2022-04-01.
+//
+
+import SwiftUI
+
+@main
+struct Scout_InsightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
