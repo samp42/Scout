534b912787a072a857ecb9e71f0a919b1d56aebc
Samuel Proulx <samuelproulx26@gmail.com>
2022-04-07 22:08:00 -0400

    WORKING SCOUT INSIGHTS

--- Scout Insights/Scout Insights/views/sheets/SheetsPageView.swift
+++ Scout Insights/Scout Insights/views/sheets/SheetsPageView.swift
@@ -6,29 +6,80 @@
 //
 
 import SwiftUI
+import CodeScanner
+
+class ScanningData: ObservableObject{
+    @Published var showScanner: Bool = false
+    @Published var sheets: [ScoutingSheet] = []
+}
 
 struct SheetsPageView: View {
     @EnvironmentObject var appState: AppState
-    var scanningData: ScanningData
+    @StateObject var scanningData = ScanningData()
+    @State private var scanningErrorMessage: String? = nil
     
     var body: some View {
-        NavigationView{
-            List {
-                ForEach($appState.teams, id: \.self) {$team in
-                    NavigationLink(destination: SheetsDetailsView(team: team, sheets: appState.getScoutingSheetsOfTeam(team: team))) {
-                        SheetsListCellView(team: team)
+        ZStack{
+            NavigationView{
+                List {
+                    ForEach($appState.teams, id: \.self) {$team in
+                        NavigationLink(destination: SheetsDetailsView(team: team, sheets: appState.getScoutingSheetsOfTeam(team: team))) {
+                            SheetsListCellView(team: team)
+                        }
                     }
-                }
-            }.navigationTitle(Text("Scouting Sheets"))
+                }.navigationTitle(Text("Scouting Sheets"))
+            }
+            ScanButtonView(scanningData: scanningData)
+        }.sheet(isPresented: $scanningData.showScanner) {
+            CodeScannerView(codeTypes: [.qr], simulatedData: "test", completion: handleScan)
         }
+    }
+    
+    func handleScan(result: Result<ScanResult, ScanError>) {
+        scanningData.showScanner = false
+        
+        switch result {
+        case .success(let result):
+            let details = result.string.components(separatedBy: ",")
+            
+            // expect 18 fields
+            guard details.count == 18 else {
+                self.scanningErrorMessage = "Received Unusual Object"
+                return
+            }
+            
+            var dict: [String: Any] = [:]
+            
+            for field in details {
+                let pair = field.components(separatedBy: ":")
+                
+                dict.updateValue(
+                    pair[1].trimmingCharacters(in: CharacterSet(charactersIn: "{ }")),
+                    forKey: pair[0].trimmingCharacters(in: CharacterSet(charactersIn: "{ }")))
+            }
+            
+            var scoutingSheet: ScoutingSheet?
+            
+            do{
+                scoutingSheet = try ScoutingSheet(JSON: dict) ?? nil
+                
+                appState.addScoutingSheet(scoutingSheet: scoutingSheet!)
+                scanningData.sheets.append(scoutingSheet!)
+            } catch {
+                self.scanningErrorMessage = "Failed to create ScoutingSheet"
+            }
+            
+        case .failure(_):
+            self.scanningErrorMessage = "Failed to read QR code."
+        }
+        
     }
 }
 
 struct SheetsPageView_Previews: PreviewProvider {
     static let appState = AppState()
-    static let scanningData = ScanningData()
     
     static var previews: some View {
-        SheetsPageView(scanningData: scanningData).environmentObject(appState)
+        SheetsPageView().environmentObject(appState)
     }
 }
