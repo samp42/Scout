f4416a34589f0ae31e82ef9c310718225734ec81
Samuel Proulx <samuelproulx26@gmail.com>
2022-04-08 12:23:21 -0400

    correct decrement bug

---
+++ Scout Insights/Scout Insights/views/robot/RobotsPageView.swift
@@ -0,0 +1,20 @@
+//
+//  RobotsPageView.swift
+//  Scout Insights
+//
+//  Created by Samuel Proulx on 2022-04-08.
+//
+
+import SwiftUI
+
+struct RobotsPageView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct RobotsPageView_Previews: PreviewProvider {
+    static var previews: some View {
+        RobotsPageView()
+    }
+}
